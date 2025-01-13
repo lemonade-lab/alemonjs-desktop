28f1647b2f8fe438cf13801114aceccaec893ce0
ningmengchongshui <ningmengchongshui@gmail.com>
2025-01-13 17:28:32 +0800

    feat: 微调

--- src/views/Expansions/App.tsx
+++ src/views/Expansions/App.tsx
@@ -4,6 +4,7 @@ import { fetchPackageInfo } from './api'
 import logoURL from '@src/assets/logo.jpg'
 
 export default function Expansions() {
+  //
   const [packageInfo, setPackageInfo] = useState<{
     'name': string
     'description': string
@@ -13,11 +14,28 @@ export default function Expansions() {
     'time': { modified: string }
     'readme': string
   } | null>(null)
+
   const [readme, setReadme] = useState('')
 
   const extensions = [
     {
       name: '@alemonjs/gui',
+      description: '测试机器人'
+    },
+    {
+      name: '@alemonjs/qq',
+      description: '测试机器人'
+    },
+    {
+      name: '@alemonjs/qq-bot',
+      description: '测试机器人'
+    },
+    {
+      name: '@alemonjs/discord',
+      description: '测试机器人'
+    },
+    {
+      name: '@alemonjs/kook',
       description: '测试机器人'
     }
   ]
@@ -38,23 +56,23 @@ export default function Expansions() {
         <div className="flex flex-col flex-1 bg-[var(--primary-bg-front)]">
           {packageInfo ? (
             <div>
-              <div className="p-2 bg-white flex gap-4 items-start">
-                <div className="w-16 h-16 flex-shrink-0 bg-gray-100 rounded-lg flex items-center justify-center">
+              <div className="p-2 bg-white flex items-center justify-center gap-4 border-b">
+                <div className="bg-gray-100 flex items-center justify-center">
                   <img
                     src={logoURL}
                     alt={`${packageInfo.name} logo`}
-                    className="w-full h-full object-cover rounded-lg"
+                    className="size-20 rounded-md"
                   />
                 </div>
-                <div className="flex-1 flex flex-col gap-2">
-                  <div className="text-2xl font-bold text-gray-800">{packageInfo.name}</div>
+                <div className="flex-1 flex flex-col gap-1">
+                  <div className="text-xl font-bold text-gray-800">{packageInfo.name}</div>
                   <div className="text-gray-700">{packageInfo.description}</div>
                   <div className="text-sm">
                     <span className="text-gray-600">版本:</span> {packageInfo['dist-tags'].latest}
                   </div>
                 </div>
               </div>
-              <div className="bg-white overflow-auto scrollbar h-[calc(100vh-9.2rem)]">
+              <div className="bg-white overflow-auto scrollbar h-[calc(100vh-8.2rem)]">
                 <Markdown source={readme} />
               </div>
             </div>
@@ -66,11 +84,11 @@ export default function Expansions() {
             </div>
           )}
         </div>
-        <nav className="min-w-60 border-l flex gap-1 flex-col p-2">
+        <nav className="w-64 xl:w-72 border-l flex gap-1 flex-col p-2">
           <div className="">
             <input
               placeholder="在应用商店中搜索扩展"
-              className="w-full px-2 py-1 text-sm rounded-sm"
+              className="w-full px-2 py-1 text-[0.7rem] rounded-sm"
             />
           </div>
           <div className="flex-1 flex flex-col gap-1">
@@ -78,7 +96,7 @@ export default function Expansions() {
               <div
                 key={index}
                 onClick={() => handlePackageClick(item.name)}
-                className="cursor-pointer flex gap-1 border p-1 flex-row h-14 justify-between items-center hover:bg-slate-200"
+                className="cursor-pointer flex gap-1  p-1 flex-row h-14 justify-between items-center hover:bg-slate-200"
               >
                 <div className="size-10 rounded-sm">
                   <img
@@ -88,8 +106,8 @@ export default function Expansions() {
                   />
                 </div>
                 <div className="flex flex-1 flex-col">
-                  <div>{item.name}</div>
-                  <div>{item.description}</div>
+                  <div className="text-[0.9rem]">{item.name}</div>
+                  <div className="text-[0.6rem]">{item.description}</div>
                 </div>
               </div>
             ))}
